652429d0edbb775e75be25ed86afe4071e4a35d8
Michael Lee <michael.l33.1994@gmail.com>
2020-09-28 20:04:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.with_each_object({}) do |(key, value), array|
+  final_results = data.with_each_object({}) do |(key, value), array|
     value.each do |inner_key, names| 
       names.each do |name|
         if !array[name]
